848d7302a21fce62388359535f9b71c72a7e1abb
Stanislaw Gwara <sgwara@gmail.com>
2022-12-28 08:58:11 +0100

    Hilt dependencies

--- app/src/main/java/com/stanleyguevara/dustytools/MainActivity.kt
+++ app/src/main/java/com/stanleyguevara/dustytools/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.stanleyguevara.dustytools.ui.theme.DustyToolsTheme
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,5 +40,21 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     DustyToolsTheme {
         Greeting("Android")
+    }
+}
+
+fun main() {
+    runBlocking {
+        val channel = Channel<Int>()
+        launch {
+            for (x in 1..5) {
+                channel.send(x * x)
+                delay(1000)
+            }
+            channel.close() // we're done sending
+        }
+        // here we print received values using `for` loop (until the channel is closed)
+        for (y in channel) println(y)
+        println("Done!")
     }
 }
