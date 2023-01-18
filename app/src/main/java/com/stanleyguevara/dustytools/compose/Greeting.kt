c593c14d03a9cee14ea5933e0ad8f5bf8f041012
Stanislaw Gwara <sgwara@gmail.com>
2023-01-18 15:27:17 +0100

    Some funky animation

--- app/src/main/java/com/stanleyguevara/dustytools/compose/Greeting.kt
+++ app/src/main/java/com/stanleyguevara/dustytools/compose/Greeting.kt
@@ -1,23 +1,28 @@
 package com.stanleyguevara.dustytools.compose
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.stanleyguevara.dustytools.R
 import com.stanleyguevara.dustytools.ui.theme.DustyToolsTheme
 import timber.log.Timber
 
@@ -49,6 +54,7 @@ fun Greetings(
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun Greeting(name: String, onClick: () -> Unit) {
     Surface(
@@ -58,28 +64,51 @@ private fun Greeting(name: String, onClick: () -> Unit) {
         var isExpanded by rememberSaveable { mutableStateOf(false) }
 
         val extraPadding by animateDpAsState(
-            targetValue = if (isExpanded) 128.dp else 56.dp,
+            targetValue = if (isExpanded) 128.dp else 0.dp,
             animationSpec = spring(
-                dampingRatio = Spring.DampingRatioHighBouncy,
+                dampingRatio = Spring.DampingRatioNoBouncy,
                 stiffness = Spring.StiffnessMedium,
             )
         )
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)) {
-            Column(Modifier
-                .weight(1.0f)
-                .padding(bottom = extraPadding)
+        Box(
+            modifier = Modifier.padding(24.dp),
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = "Hello")
-                Text(text = name)
+                Column(
+                    Modifier
+                        .weight(1.0f)
+                        .padding(bottom = extraPadding)
+                ) {
+                    Text(text = "Hello")
+                    Text(text = name)
+                }
+                ElevatedButton(onClick = {
+                    isExpanded = isExpanded.not()
+                    onClick.invoke()
+                }) {
+                    Text(text = if (isExpanded) "Show less" else "Show more")
+                }
             }
-            ElevatedButton(onClick = {
-                isExpanded = isExpanded.not()
-                onClick.invoke()
-            }) {
-                Text(text = if (isExpanded) "Show less" else "Show more")
+            AnimatedVisibility(
+                enter = scaleIn(animationSpec = tween(durationMillis = 100, delayMillis = 200)),
+                exit = scaleOut(animationSpec = tween(durationMillis = 100, delayMillis = 0)),
+                visible = isExpanded
+            ) {
+                IconButton(onClick = {
+                    isExpanded = isExpanded.not()
+                    onClick.invoke()
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "Collapse icon",
+                        tint = MaterialTheme.colorScheme.surface,
+                    )
+                }
+
             }
         }
     }
